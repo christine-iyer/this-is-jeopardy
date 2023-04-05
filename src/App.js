ce5ecbd44cc5b4a0560251f75ce3d491e568692f
chrisIyer <hellohotbox420@gmail.com>
2023-04-05 17:51:46 -0400

    Co-authored-by: Michael Arthur <arthurmichaelw@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ export default function App() {
       setScore(score + jeopardyQuest.value);
     }
   }
+  
 
   const getDecrementPoints = () => {
     if (jeopardyQuest && jeopardyQuest.value) {
